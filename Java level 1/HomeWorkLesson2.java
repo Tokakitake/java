d656c4f53cefe18be983b1a4672a2e8ed433f7e6
Tokakitake <65546504+Tokakitake@users.noreply.github.com>
2021-12-20 14:17:00 +0300

    Update HomeWorkLesson2.java

--- Java level 1/HomeWorkLesson2.java
+++ Java level 1/HomeWorkLesson2.java
@@ -5,46 +5,46 @@
  */
 public class HomeWorkLesson2 {
     public static void main(String[] args) {
-        System.out.println(Between10And20(7, 7));
-        System.out.println(Between10And20(0, 10));
-        System.out.println(Between10And20(7, 77));
+        System.out.println(between10And20(7, 7));
+        System.out.println(between10And20(0, 10));
+        System.out.println(between10And20(7, 77));
 
-        NegativeOrPositive(0);
-        NegativeOrPositive(-5);
-        NegativeOrPositive(5);
+        negativeOrPositive(0);
+        negativeOrPositive(-5);
+        negativeOrPositive(5);
 
-        System.out.println(Negative(0));
-        System.out.println(Negative(-11));
-        System.out.println(Negative(11));
+        System.out.println(negative(0));
+        System.out.println(negative(-11));
+        System.out.println(negative(11));
 
-        RepeatString("Milk inside a bag of", 6);
+        repeatString("Milk inside a bag of", 6);
 
-        System.out.println(Years(2000));
-        System.out.println(Years(1914));
-        System.out.println(Years(1945));
-        System.out.println(Years(1998));
-        System.out.println(Years(1516));
+        System.out.println(years(2000));
+        System.out.println(years(1914));
+        System.out.println(years(1945));
+        System.out.println(years(1998));
+        System.out.println(years(1516));
     }
 
-    static boolean Between10And20(int a, int b) {
+    static boolean between10And20(int a, int b) {
         return a + b >= 10 && a + b <= 20 ;
     }
 
-    static void NegativeOrPositive (int a) {
+    static void negativeOrPositive (int a) {
         System.out.println(a >= 0? "Positive" : "Negative");
     }
 
-    static boolean Negative(int a) {
+    static boolean negative(int a) {
         return a < 0;
     }
 
-    static void RepeatString(String s, int count) {
+    static void repeatString(String s, int count) {
         for (int i = 0; i < count; i++ ) {
             System.out.println(s);
         }
     }
 
-    static boolean Years(int y) {
+    static boolean years(int y) {
         return y % 400 == 0 || (y % 4 == 0 && y % 100 != 0);
     }
 }
