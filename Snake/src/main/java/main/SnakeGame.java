8e94aafd3d7164501353bf81b44855374996caef
Tokakitake <65546504+Tokakitake@users.noreply.github.com>
2022-01-06 17:27:24 +0300

    Add files via upload

---
+++ Snake/src/main/java/main/SnakeGame.java
@@ -0,0 +1,39 @@
+package main;
+
+/**
+ * Java level 1 HomeWork 8
+ * @author Muratov Nail
+ * @version 05.01.2022
+ */
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class SnakeGame extends JFrame {
+
+    public SnakeGame() {
+        
+        initScreen();
+    }
+    
+    private void initScreen() {
+        
+        add(new ScreenPanel());
+               
+        setResizable(false);
+        pack();
+        
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+
+    public static void main(String[] args) {
+        
+        EventQueue.invokeLater(() -> {
+            JFrame frame = new SnakeGame();
+            frame.setVisible(true);
+        });
+    }
+}
